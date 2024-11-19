8dbfd67a0151875bbd6d50d7f2804ac2511ed7ef
BusAlena <alenkabusygina8507@gmail.com>
2024-11-19 18:05:36 +0300

    Add files via upload

---
+++ Peremennye.py
@@ -0,0 +1,5 @@
+amount_of_homework=12
+amount_of_time_spent=1.5
+name_of_coure= "Python"
+time_for_one_lesson= (amount_of_time_spent / amount_of_homework)
+print('Курс:',name_of_coure, ", всего задач:",amount_of_homework, ", затрачено часов:",amount_of_time_spent, ", среднее время выполнения:",time_for_one_lesson)
